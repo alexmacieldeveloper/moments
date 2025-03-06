8e27ed9fb8b7a3a510f849038d333eda21413471
Álex Maciel <aweasley.gina@gmail.com>
2025-03-06 19:40:02 -0300

    calling message component and redirecting route

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment/moment.service';
+import { MessagesService } from 'src/app/services/messages/messages.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-moment',
@@ -12,7 +14,7 @@ export class NewMomentComponent implements OnInit {
   btnText = 'Compartilhar!';
   image?: File;
   
-  constructor(private momentService: MomentService) { }
+  constructor(private momentService: MomentService, private messagesService: MessagesService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -28,5 +30,9 @@ export class NewMomentComponent implements OnInit {
     }
 
     await this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add(`Momento adicionado com sucesso!`);
+
+    this.router.navigate(['/']);
   }
 }
