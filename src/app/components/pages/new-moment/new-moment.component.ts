8152f38087505fba1e0f7d431f0efb7b2b8f561d
Álex Maciel <aweasley.gina@gmail.com>
2025-03-06 15:46:39 -0300

    integrating form with api

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment/moment.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrls: ['./new-moment.component.css']
 })
+
 export class NewMomentComponent implements OnInit {
   btnText = 'Compartilhar!';
+  image?: File;
   
-  constructor() { }
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
   }
 
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append("title", moment.title);
+    formData.append("description", moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image)
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+  }
 }
