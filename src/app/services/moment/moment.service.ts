f649ec9b2d1d496409bd3709cbd0cce2addc5f3c
Álex Maciel <aweasley.gina@gmail.com>
2025-03-06 20:27:58 -0300

    creating service  from exibicion of moments

--- src/app/services/moment/moment.service.ts
+++ src/app/services/moment/moment.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Moment } from 'src/app/Moment';
+import { Response } from 'src/app/Response';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -15,7 +16,13 @@ export class MomentService {
 
   constructor(private http: HttpClient) { }
 
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
+
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
   }
+
+
 }
